befedd62334f8ef20bbda0429201dcbe9894bb2c
Mathias Løvnes <mathiaslovnes02@gmail.com>
2025-09-19 00:26:50 +0200

    Plotting now works for tmux but background colors and label sizes are still fucked - vim-jukit

--- private_dot_config/nvim/lua/plugins/vim-jukit.lua
+++ private_dot_config/nvim/lua/plugins/vim-jukit.lua
@@ -4,29 +4,11 @@ return {
   config = function()
     vim.g.jukit_terminal = 'tmux' -- Optional: Set terminal for plotting
     vim.g.jukit_use_uberzug = 1
-    vim.g.jukit_python_os_cmd = 'ipython'
-    vim.g.jukit_output_new_os_window = 0
-    vim.g.jukit_outhist_new_os_window = 0
-    vim.g.jukit_matplotlib_backend = 'module://matplotlib-backend-kitty'
-    vim.g.jukit_mpl_block = 0
-    -- Highlight cell markers
-    -- vim.g.jukit_highlight_markers = 1
-    -- You can define the colors after loading your colorscheme:
-    -- vim.cmd 'highlight jukit_cellmarker_colors guifg=#0000ff guibg=NONE'
-    -- Highlight background of textcells
-    vim.g.jukit_enable_textcell_bg_hl = 1
-    -- You can define the background color like this:
-    -- vim.cmd 'highlight jukit_textcell_bg_colors guibg=#131628 ctermbg=0'
-    vim.g.jukit_text_syntax_file = '' -- empty disables legacy syntax
+    vim.g.jukit_python_os_cmd = 'ipython3'
 
-    -- Enable markdown syntax highlighting in textcells
-    vim.g.jukit_enable_textcell_syntax = 1
+    -- trying this out
 
-    -- Enable highlighting for all extensions
-    vim.g.jukit_hl_ext_enabled = '*'
-
-    vim.g.jukit_ueberzug_pos = { 0.0, 0.0, 1.0, 1.0 } -- Lua table syntax (no [])
-    vim.g.jukit_ueberzug_pos_noout = { 0.0, 0.0, 1.0, 1.0 }
+    ----------
 
     vim.g.jukit_layout = {
       split = 'horizontal',
